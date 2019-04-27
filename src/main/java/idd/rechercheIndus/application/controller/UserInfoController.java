9b3d74d3aa6e042f00256bcc3276e3de33647cc9
unknown <pierre-andre.geulin@hotmail.fr>
2019-04-27 18:54:05 +0200

    mise à jour affichage

--- src/main/java/idd/rechercheIndus/application/controller/UserInfoController.java
+++ src/main/java/idd/rechercheIndus/application/controller/UserInfoController.java
@@ -3,19 +3,16 @@ package idd.rechercheIndus.application.controller;
 import java.util.Optional;
 
 import javax.mail.MessagingException;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import idd.rechercheIndus.application.entity.UserAuth;
 import idd.rechercheIndus.application.entity.UserInfo;
 import idd.rechercheIndus.application.exception.InvalidDataException;
 import idd.rechercheIndus.application.exception.UserInfoNotFoundException;
 import idd.rechercheIndus.application.service.MailService;
-import idd.rechercheIndus.application.service.UserAuthService;
 import idd.rechercheIndus.application.service.UserInfoService;
 import javassist.NotFoundException;
 
@@ -26,20 +23,25 @@ public class UserInfoController {
 	private UserInfoService userInfoService;
 	
 	@Autowired
-	private UserAuthService userAuthService;
-	
-	@Autowired
 	private MailService mailService;
 	
-	@PostMapping("/login/{mail}")
-	public UserInfo login(@PathParam("mail") String mail) throws NotFoundException {
+	@PostMapping("/login")
+	public UserInfo login(@RequestBody UserInfo userInfo) throws NotFoundException {
+		if(userInfo == null) {
+			throw new UserInfoNotFoundException("");
+		}
+		String mail = userInfo.getMail();
 		if(mail == null) {
 			throw new UserInfoNotFoundException("");
 		}
-		
 		Optional<UserInfo> userInfoOpt = userInfoService.findUserByMail(mail);
 		if(userInfoOpt.isPresent()) {
-			return userInfoOpt.get();
+			if(userInfoOpt.get().isEnabled()) {
+				return userInfoOpt.get();
+			}
+			else {
+				return null;
+			}
 		}
 		else {
 			throw new UserInfoNotFoundException("");
@@ -59,27 +61,6 @@ public class UserInfoController {
 		}
 		else {
 			throw new InvalidDataException("");
-		}
-	}
-	
-	@PostMapping("/login")
-	public UserInfo login(@RequestBody UserAuth userAuth) throws NotFoundException {
-		if(userAuth == null) {
-			throw new UserInfoNotFoundException("");
-		}
-		
-		Optional<UserAuth> userAuthOpt = userAuthService.findUserAuthByMailAndPassword(userAuth.getMail(), userAuth.getPassword());
-		if(userAuthOpt.isPresent()) {
-			Optional<UserInfo> userInfoOpt = userInfoService.findUserByMail(userAuth.getMail());
-			if(userInfoOpt.isPresent()) {
-				return userInfoOpt.get();
-			}
-			else {
-				throw new UserInfoNotFoundException("");
-			}
-		}
-		else {
-			throw new UserInfoNotFoundException("");
 		}
 	}
 	
